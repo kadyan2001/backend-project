1192be3ddd0f56ac141d23a9b75fc3c8331b539c
Manish Kadyan <mankashkadian@gmail.com>
2024-05-29 11:01:38 +0530

    new commit

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js"
+
+
+const connectDB=async()=>{
+
+    try{
+        const connectionInstance= mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+    console.log(`/n MongoDB connected !! DB HOST:${(await connectionInstance).connection.host}`);
+    }
+    catch(error){
+        console.log("MONGODB connenction error",error);
+        process.exit(1)
+    }
+
+}
+
+
+export default connectDB
